f71c910f8865275bb3588dc931d6476e0ac21c06
aminaelh <aminaelh.55.5@gmail.com>
2025-12-06 15:45:56 +0100

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '' }
+];
